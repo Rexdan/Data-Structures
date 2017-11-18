9b23aa1705af82e98bbb92b6b7f8333d320a29e2
Rexdan <andrefigueiredopereira@gmail.com>
2017-11-18 15:35:40 -0500

    Added linear package.

---
+++ Data Structures/src/linear/IntNode.java
@@ -0,0 +1,15 @@
+package linear;
+
+public class IntNode {
+	public int data;
+	public IntNode next;
+	public IntNode(int data, IntNode next) {
+		this.data = data;
+		this.next = next;
+	}
+
+	public String toString() {
+		return data + "";
+	}
+	
+}
